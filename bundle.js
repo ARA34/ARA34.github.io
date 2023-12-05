bd5cb73cc9aecc9e811396d44bd4eeccafc7243b
ARA34 <agreyesaranda@gmail.com>
2023-12-04 21:53:53 -0800

    trying out the hasSay command

--- bundle.js
+++ bundle.js
@@ -491,7 +491,7 @@ module.exports = class {
         this.requirements.longScript = { bool: false, str: 'At least one script with 4 blocks' };//done
         this.requirements.events = { bool: false, str: 'At least one sprite with three different event blocks.' };//done
         this.requirements.loop = { bool: false, str: 'At least one sprite uses a repeat block to move smoothly.' };//done
-        this.requirements.hasThink = { bool: false, str: "Has a say block" };
+        this.requirements.hasSay = { bool: false, str: "Has a say block" };
     }
 
 
@@ -505,19 +505,20 @@ module.exports = class {
        let sprites = project.targets.filter(t=> !t.isStage);
 
         function procSprite(sprite){
-            var out = {maxScriptLength:0, nEvents:0, hasLoop:false, hasThink: false }
-            for(let target of project.targets){
-                if(target.isStage) {continue}
-                else{
-                    for(let script of target.scripts) {
-                        for(let block of script.allBlocks()){
-                            if((block.opcode == "looks_sayforsecs")) {
-                                out.hasThink = true
-                            }
-                        }
+            var out = {maxScriptLength:0, nEvents:0, hasLoop:false, hasSay: false }
+
+            //target is sprite, project.targets is all sprites
+            //script is scriptcode of target sprite, target.scripts is all scripts in the target
+            //block is specific block in script, script.allBlocks() is all blocks in the script
+
+            for (let script of sprite.scripts){
+                for (let block of script.allBlocks()){
+                    if((block.opcode == "looks_sayforsecs")){
+                        out.hasSay = true;
                     }
                 }
             }
+            
             out.maxScriptLength = Math.max(...sprite.scripts.filter(s=> s.blocks[0].opcode.includes("event_")).map(s=>s.blocks.length));
             out.nEvents = new Set(sprite.scripts.filter(s=> s.blocks[0].opcode.includes("event_")).filter(s=>s.blocks.length > 1).map(s=>s.blocks[0].opcode + JSON.stringify(s.blocks[0].fields))).size;
             var loops = sprite.scripts.filter(s=> s.blocks[0].opcode.includes("event_")).map(s=>s.blocks.filter(b=>b.opcode.includes("control_repeat"))).flat();
@@ -530,9 +531,11 @@ module.exports = class {
         this.requirements.longScript.bool = Math.max(...results.map(r=>r.maxScriptLength)) >= 4; //checking for long script
         this.requirements.events.bool = results.filter(r=>r.nEvents >= 3).length > 0;
         this.requirements.loop.bool = results.some(r=>r.hasLoop);
+        this.requirements.hasSay.bool = results.some(b=>b.hasSay);
         // this.reqiuirements.loop.bool = results.some(r=>r.hasLoop);
         console.log(results);
         console.log(results.some(r=>r.hasLoop));
+        console.los(results.some(b=>b.hasSay));
         console.log(sprites);
    
 
