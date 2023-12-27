8ed0ce1f19e0a4d7f8fcb6f0921b257070fbc2a8
ARA34 <agreyesaranda@gmail.com>
2023-12-26 23:49:30 -0800

    Another way to find say and sound

--- act3-grading-scripts/systems.js
+++ act3-grading-scripts/systems.js
@@ -3,11 +3,10 @@ Place holder code for systems grading script
 Adapted from final-project.js
 
 
-This script account for blocks being connected but doesnt account for the type of blocks being.. 
+This script account for blocks being connected but doesnt account for the type of blocks being... 
 connected except for the first one which is the run block
 */
 
-// check for connecting blocks
 
 require('../grading-scripts-s3/scratch3')
 
@@ -21,7 +20,8 @@ module.exports = class {
         this.requirements.longScript = { bool: false, str: 'At least one script with 4 blocks' };//done
         this.requirements.events = { bool: false, str: 'At least one sprite with three different event blocks.' };//done
         this.requirements.loop = { bool: false, str: 'At least one sprite uses a repeat block to move smoothly.' };//done
-        this.requirements.hasSay = { bool: false, str: "Has a say block" };
+        this.requirements.hasSay = { bool: false, str: "Has a say block" }; // example -- can remove
+        this.requirements.explains = { bool: false, str: "At least sound block or say block to explain."}; //working
     }
 
 
@@ -32,27 +32,25 @@ module.exports = class {
 
         let stage = project.targets.find(t => t.isStage);
      
-       let sprites = project.targets.filter(t=> !t.isStage);
+       let sprites = project.targets.filter(t=> !t.isStage); //important
 
         function procSprite(sprite){
-            var out = {maxScriptLength:0, nEvents:0, hasLoop:false, hasSay: false }
+            var out = {maxScriptLength:0, nEvents:0, hasLoop:false, hasSay: false , hasExplanation: false , explains: false };
 
-            //target is sprite, project.targets is all sprites
-            //script is scriptcode of target sprite, target.scripts is all scripts in the target
-            //block is specific block in script, script.allBlocks() is all blocks in the script
-
-            for (let script of sprite.scripts){
+            /*for (let script of sprite.scripts){
                 for (let block of script.allBlocks()){
                     if((block.opcode == "looks_sayforsecs")){
                         out.hasSay = true;
                     }
                 }
-            }
+            }*/
             
             out.maxScriptLength = Math.max(...sprite.scripts.filter(s=> s.blocks[0].opcode.includes("event_")).map(s=>s.blocks.length));
             out.nEvents = new Set(sprite.scripts.filter(s=> s.blocks[0].opcode.includes("event_")).filter(s=>s.blocks.length > 1).map(s=>s.blocks[0].opcode + JSON.stringify(s.blocks[0].fields))).size;
             var loops = sprite.scripts.filter(s=> s.blocks[0].opcode.includes("event_")).map(s=>s.blocks.filter(b=>b.opcode.includes("control_repeat"))).flat();
             out.hasLoop = loops.some(loop=>loop.subscripts.some(s=>s.blocks.some(block=>block.opcode.includes("motion_") && s.blocks.some(block=>block.opcode.includes("control_wait")))));
+            out.hasExplanation = sprite.scripts.some(s=>s.blocks.some(block=>block.opcode.includes("looks_sayforsecs") || s.blocks.some(block=>block.opcode.includes("sound_playuntildone")))); // either sound or say block
+            out.hasSay = sprite.scripts.some(blocks=>blocks.some(block=>block.opcode == "looks_sayforsecs"));
             return out;
         }
 
@@ -61,13 +59,15 @@ module.exports = class {
         this.requirements.longScript.bool = Math.max(...results.map(r=>r.maxScriptLength)) >= 4; //checking for long script
         this.requirements.events.bool = results.filter(r=>r.nEvents >= 3).length > 0;
         this.requirements.loop.bool = results.some(r=>r.hasLoop);
-        this.requirements.hasSay.bool = results.some(b=>b.hasSay);
         // this.reqiuirements.loop.bool = results.some(r=>r.hasLoop);
+
+        this.requirements.explains.bool = results.filter(c=>c.hasExplanation).length = 6;
+        this.requirements.hasSay.bool = results.some(b=>b.hasSay);
+
         console.log(results);
         console.log(results.some(r=>r.hasLoop));
-        console.los(results.some(b=>b.hasSay));
-        console.log(sprites);
-   
+        console.log(results.filter(c=>c.hasExplanation).length = 6);
+        console.log(results.some(b=>b.hasSay));
 
         return;
     }
