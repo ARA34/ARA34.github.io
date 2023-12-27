130d4a193dc43c17f68d4a506740b4d97bd8df46
ARA34 <agreyesaranda@gmail.com>
2023-12-27 01:21:23 -0800

    cleaned up systems arrow code

--- act3-grading-scripts/systems.js
+++ act3-grading-scripts/systems.js
@@ -19,7 +19,6 @@ module.exports = class {
         this.requirements.longScript = { bool: false, str: 'At least one script with 4 blocks' };//done
         this.requirements.events = { bool: false, str: 'At least one sprite with three different event blocks.' };//done
         this.requirements.loop = { bool: false, str: 'At least one sprite uses a repeat block to move smoothly.' };//done
-        this.requirements.hasSay = { bool: false, str: "Has a say block" }; // example -- can remove
         this.requirements.explains = { bool: false, str: "At least sound block or say block to explain each arrow(6)."}; //done
     }
 
@@ -34,25 +33,15 @@ module.exports = class {
        let sprites = project.targets.filter(t=> !t.isStage); //important
 
         function procSprite(sprite){
-            var out = {maxScriptLength:0, nEvents:0, hasLoop:false, hasSay:false , hasExplanation:false , explains:false };
-
-            /*for (let script of sprite.scripts){
-                for (let block of script.allBlocks()){
-                    if((block.opcode == "looks_sayforsecs")){
-                        out.hasSay = true;
-                    }
-                }
-            }*/
+            var out = {maxScriptLength:0, nEvents:0, hasLoop:false, hasExplanation:false, explains:false};
             
             out.maxScriptLength = Math.max(...sprite.scripts.filter(s=> s.blocks[0].opcode.includes("event_")).map(s=>s.blocks.length));
             out.nEvents = new Set(sprite.scripts.filter(s=> s.blocks[0].opcode.includes("event_")).filter(s=>s.blocks.length > 1).map(s=>s.blocks[0].opcode + JSON.stringify(s.blocks[0].fields))).size;
             var loops = sprite.scripts.filter(s=> s.blocks[0].opcode.includes("event_")).map(s=>s.blocks.filter(b=>b.opcode.includes("control_repeat"))).flat();
             out.hasLoop = loops.some(loop=>loop.subscripts.some(s=>s.blocks.some(block=>block.opcode.includes("motion_") && s.blocks.some(block=>block.opcode.includes("control_wait")))));
             out.hasExplanation = sprite.scripts.some(s=>s.blocks.some(block=>block.opcode.includes("looks_sayforsecs") || s.blocks.some(block=>block.opcode.includes("sound_playuntildone")))); // either sound or say block
-            out.hasSay = sprite.scripts.some(s=>s.blocks.some(block=>block.opcode == "looks_sayforsecs"));
             return out;
         }
-
 
         var results = sprites.map(procSprite);
         this.requirements.longScript.bool = Math.max(...results.map(r=>r.maxScriptLength)) >= 4; //checking for long script
@@ -60,12 +49,9 @@ module.exports = class {
         this.requirements.loop.bool = results.some(r=>r.hasLoop);
         // this.reqiuirements.loop.bool = results.some(r=>r.hasLoop);
         this.requirements.explains.bool = (results.length >= 6) ? this.requirements.explains.bool = results.filter(c=>c.hasExplanation == true).length == 6 : false;
-        this.requirements.hasSay.bool = results.some(b=>b.hasSay);
 
         console.log(results);
         console.log(results.some(r=>r.hasLoop));
-        //console.log(results.filter(c=>c.hasExplanation == true).length == 6);
-        console.log(results.some(b=>b.hasSay));
 
         return;
     }
