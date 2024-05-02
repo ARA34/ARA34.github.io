8a8020a7995917e04a3c5cf9f6c338ccb4cd7785
ARA34 <agreyesaranda@gmail.com>
2024-05-02 00:41:54 -0700

    minor bug fix

--- bundle.js
+++ bundle.js
@@ -1988,7 +1988,7 @@ Place holder code for systems grading script
 Adapted from final-project.js
 */
 
-require('../grading-scripts-s3/scratch3')
+require('./scratch3');
 
 module.exports = class {
     constructor() {
