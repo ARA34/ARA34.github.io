d48073282aa6c0307296494c6f9ed080f363d5e7
ARA34 <agreyesaranda@gmail.com>
2024-07-03 22:07:05 -0700

    Update Madlibs project

--- bundle.js
+++ bundle.js
@@ -2056,25 +2056,54 @@ module.exports = class {
     }
 }
 },{"./scratch3":26}],13:[function(require,module,exports){
-// Madlibs Test
+// madlibs.js test
+//Madlibs Test
 require('./scratch3');
 
 // identifty a varibale and print out its value
 
 module.exports = class{
-    constructor() {
+    constructor(){
         this.requirements = {};
         this.extensions = {};
     }
-    initReqs() {
-        this.requirements.hasVar = {bool: false, str: "I had the given variable"};
+    initReqs(){
+
+        this.requirements.Sprites = { bool: false, str: "I had at least one sprite and a backdrop" };
+        this.requirements.VarsExistance = { bool: false, str: "I created 3 variables" };
+        this.requirements.initVars = { bool: false, str: "I initialized my varibale values to 0" };
+        this.requirements.questionsAndVars = {bool: false, str: "I asked questions and store their responses in my variables" };
     }
 
-    grade(fileObj, user) {
+    grade(fileObj, user){
+        //grading function
         var project = new Project(fileObj, null);
         this.initReqs();
         if (!is(fileObj)) return;
-        console.log("targets: ", project.targets);
+        
+        let allSprites = project.targets;
+        let stage = project.targets.find(t => t.isStage);
+        let sprites = project.targets.filter(t => !t.isStage);
+
+        function procSprite(sprite){
+            //evaluate a single sprite
+            var out = {};
+            return out;
+        }
+        NumVars = 0
+        for (sprite in allSprites){
+            if (sprite.varibales){
+                // NumVars += 1;
+                console.log(type(sprite.varibales))
+                console.log("numOfVars:", len(sprite.varibales))
+            }
+        }
+
+        this.requirements.Sprites = len(allSprites) >= 2;
+        this.requirements.VarsExistance = NumVars >= 3;
+        console.log("targets: ", allSprites);
+        console.log("loopVars", NumVars);
+
         return;
     }
 }
