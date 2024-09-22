aed229130ae1272e2bdd3c0fd0b63c99e4394345
ARA34 <agreyesaranda@gmail.com>
2024-09-22 09:42:45 -0700

    dialogProject

--- bundle.js
+++ bundle.js
@@ -1837,7 +1837,7 @@ module.exports = class {
 
         let stage = project.targets.find(t=>t.isStage);
         let sprites = project.targets.filter(t=>!t.isStage);
-        out.twoSpritesPresentProject.bool = sprites.length >= 1;
+        twoSpritesPresentProject.bool = sprites.length >= 2;
 
         function collectHeads(inputScripts) {
             let headCollector = {};
