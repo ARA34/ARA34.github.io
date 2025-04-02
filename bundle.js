ac9b72cabee6d703f309c81fc68c9e8c96ef5bdb
ARA34 <agreyesaranda@gmail.com>
2025-04-02 15:59:42 -0700

    Invasive Species bug fix

--- bundle.js
+++ bundle.js
@@ -2334,19 +2334,22 @@ module.exports = class {
         this.initReqs();
         if (!is(fileObj)) return;
 
+        let spriteLibrary = []; // not in use
+
         let stage = project.targets.find(t=>t.isStage);
         let sprites = project.targets.filter(t=>!t.isStage);
-        let invasiveSprite = project.targets.find(t=>t.name.toLowerCase() == "invasive");
-        let explainerSprite = project.targets.find(t=>t.name.toLowerCase() == "sprite");
 
-        this.requirements.invasiveSprite.bool = invasiveSprite != null; //NUANCE: sprite must be named invasive
-        this.requirements.invasiveExplained.bool = (invasiveSprite != null) ? invasiveSprite.scripts.some(s=>s.blocks[0].opcode.includes("event_") && s.blocks.some(b=>b.opcode.includes("looks_say") || b.opcode.includes("sound_play"))): false; //NUANCE: Sprite must be named invasive
-        this.requirements.ecosystemExplained.bool = (explainerSprite != null) ? explainerSprite.scripts.some(s=>s.blocks[0].opcode.includes("event_") && s.blocks.some(b=>b.opcode.includes("looks_say") || b.opcode.includes("sound_play"))): false; //NUANCE: Sprite must be named sprite
+        // let invasiveSprite = project.targets.find(t=>t.name.toLowerCase() == "invasive");
+        // let explainerSprite = project.targets.find(t=>t.name.toLowerCase() == "sprite");
+
+        this.requirements.invasiveSprite.bool = sprites.length >= 1;//invasiveSprite != null;
+        this.requirements.invasiveExplained.bool = sprites.scripts.some(s=>s.blocks[0].opcode.includes("event_") && s.blocks.some(b=>b.opcode.includes("looks_say") || b.opcode.includes("sound_play")));
+        this.requirements.ecosystemExplained.bool = sprites.scripts.some(s=>s.blocks[0].opcode.includes("event_") && s.blocks.some(b=>b.opcode.includes("looks_say") || b.opcode.includes("sound_play")));
         this.requirements.backdropPresent.bool = stage.costumes.length >= 1; //NUANCE: can't tell that the image is of ecosystem
         
         console.log("-- DEBUG --");
-        console.log("IMPORTANT: Invasive sprite must be named 'invasive'");
-        console.log("IMPORTANT: Explainer sprite must be named 'sprite'");
+        // console.log("IMPORTANT: Invasive sprite must be named 'invasive'");
+        // console.log("IMPORTANT: Explainer sprite must be named 'sprite'");
 
         return;
     }
